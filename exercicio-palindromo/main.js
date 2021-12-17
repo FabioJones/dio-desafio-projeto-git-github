11d190d21a1ff77b29be0548d9f6bd5e8154e2a2
Fabio Jones <89716267+FabioJones@users.noreply.github.com>
2021-12-16 22:25:41 -0300

    exercicio-palindromo
    exercício pratico  aula-variáveis e tipos DIO

---
+++ exercicio-palindromo/main.js
@@ -0,0 +1,25 @@
+// solução 1
+
+function verificarPalindromo(string){
+    if(!string)
+        return 'string inexistente'
+
+        return string.split("").reverse().join("") === string
+    
+}
+console.log(verificarPalindromo('ovo'))
+
+// solução 2
+
+function verificarPalindromo2(string){
+    if(!string)
+    return 'string inexistente'
+
+    for(let i = 0 ; i < string.lenght / 2 ; i++){
+        if(string[i] !== string[string.lenght -1 -1]){
+            return false
+        }
+    }
+    return true
+}
+console.log(verificarPalindromo2('ovo'))
